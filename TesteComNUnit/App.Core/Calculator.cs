c4e2aa6bdefddf202af953cd38a4862e55f006d6
Henrique Soares <148624558+HenriqueAGSoares@users.noreply.github.com>
2025-10-25 00:18:23 -0300

    Upload

---
+++ TesteComNUnit/App.Core/Calculator.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+
+namespace App.Core
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b) => a + b;
+        public int Sub(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+
+        public double Div(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException("Divisor não pode ser zero.");
+            return a / b;
+        }
+    }
+}
