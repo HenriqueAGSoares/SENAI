a12bb47a1147759670b1424406ab49d5e69996f3
Henrique Soares <148624558+HenriqueAGSoares@users.noreply.github.com>
2025-11-10 10:05:16 -0300

    Add files via upload

---
+++ App.Tests/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using App.Core;
+using NUnit.Framework;
+
+namespace App.Tests;
+
+public class CalculatorTests
+{
+    private Calculator _calc = null;
+
+    [SetUp]
+    public void Setup()
+    {
+        _calc = new Calculator();
+    }
+
+    //[Test]
+    //public void Test1()
+    //{
+    //    Assert.Pass();
+    //}
+    public void Sum_DeveSomar2e3_Retorna5()
+    {
+        // Arrange
+        var a = 2;
+        var b = 3;
+
+        // Act
+        var result = _calc.Sum(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
+    }
+
+    [TestCase(1, 1, 2)]
+    [TestCase(-2, 2, 0)]
+    [TestCase(10, -3, 7)]
+    public void Sum_VariosCenarios(int a, int b, int esperado)
+    {
+        // Arrange
+        var calc = new Calculator();
+
+        // Act
+        var result = calc.Sum(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(esperado));
+    }
+    [TestCase(5, 5, 0)]
+    [TestCase(30, 20, 10)]
+    [TestCase(15, 10, 5)]
+    public void Sub_VariosCenarios(int a, int b, int esperado)
+    {
+        var calc = new Calculator(); // A
+        var result = calc.Sub(a, b); // A
+        Assert.That(result, Is.EqualTo(esperado)); // A
+    }
+    [TestCase(5, 5, 25)]
+    [TestCase(10, 5, 50)]
+    [TestCase(5, 3, 15)]
+    public void Mul_VariosCenarios(int a, int b, int esperado)
+    {
+        var calc = new Calculator(); // A
+        var result = calc.Mul(a, b); // A
+        Assert.That(result, Is.EqualTo(esperado)); // A
+    }
+    [TestCase(5, 5, 1)]
+    [TestCase(10, 2, 5)]
+    [TestCase(30, 3, 10)]
+    public void Div_VariosCenarios(int a, int b, double esperado)
+    {
+        var calc = new Calculator(); // A
+        var result = calc.Div(a, b); // A
+        Assert.That(result, Is.EqualTo(esperado)); // A
+    }
+}
---
+++ Checklist Final.md
@@ -0,0 +1,11 @@
+# Checklist Final da Atividade Sobre NUnit
+
+## Concluídos
+Instalar .NET e verificar versão com "dotnet --version".
+Criar solução, projeto App.Core e App.Tests (NUnit) pela CLI.
+Escrever testes com AAA e asserts do NUnit.
+Executar dotnet test e interpretar resultados.
+Criar testes parametrizados com [TestCase].
+
+## Opcionais Incompletos
+Aplicar TDD no método IsPrime.
